d2833a3d7e2d91177e6bfdeb89553f9382c616bd
diglopes <diego-chez@hotmail.com>
2019-07-18 07:43:23 -0300

    feat: creates the product section, fetches data from api and give it some styles to that

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const vm = new Vue({
   el: "#app",
   data: {
-    mensagem: "Olá mundo!"
+    produtos: []
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then(res => res.json())
+        .then(json => (this.produtos = json));
+    }
+  },
+  created() {
+    this.fetchProdutos();
   }
 });
