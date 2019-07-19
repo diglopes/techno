925bcd8f48adb7a84ad78b1fd1530e5ee93b5520
diglopes <diego-chez@hotmail.com>
2019-07-19 06:30:14 -0300

    feat: creates the method that gets the details of a clicked product

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const vm = new Vue({
   el: "#app",
   data: {
-    products: []
+    products: [],
+    product: false
   },
   methods: {
     fetchProducts() {
       fetch("./api/produtos.json")
         .then(res => res.json())
         .then(json => (this.products = json));
+    },
+    fetchProductDetails(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(res => res.json())
+        .then(json => (this.product = json));
     }
   },
   filters: {
